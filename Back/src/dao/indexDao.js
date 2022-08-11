005202d05943e3ebeadb3ca67996b8e6fb23ad9a
dobyming <12184874@inha.edu>
2022-08-11 12:10:27 +0900

    src full created

--- Back/src/dao/indexDao.js
+++ Back/src/dao/indexDao.js
@@ -1,84 +1,16 @@
 const { pool } = require("../../config/database");
 
-// //조회 API 
-// exports.selectMovies = async function (connection,title) {
-//   const selectAllMoviesQuery = `SELECT * FROM movieinfo;`; //실제 조회하고자하는 db쿼리문
-//   const selectMoviesByTitleQuery = `SELECT * FROM movieinfo where title = ?;`; //실제 조회하고자하는 db쿼리문
-//   const Params = [title]; //Parmas가 title인자(?로이동)로 전달됌
-
-//   let Query;
-//   if(!title){ //만약 title이 null값이라면 모든 영화정보를 조회
-//     Query = selectAllMoviesQuery;
-//   }
-//   else{ //title에 어떤값이 있다면 그 값에 대한 정보를 출력
-//     Query = selectMoviesByTitleQuery;
-//   }
-//   //let Query = selectMoviesByTitleQuery ? selectAllMoviesQuery;
-
-//   const rows = await connection.query(Query, Params);
-
-//   return rows;
-// };
-
-// //생성 API
-// exports.insertMovies = async function (connection,title,genr,director,price) {
-//   const Query = `INSERT INTO movieinfo(title, genr, director, price) VALUES (?,?,?,?);`; //실제 조회하고자하는 db쿼리문
-//   const Params = [title,genr,director,price]; //Parmas가 title인자(?로이동)로 전달됌
-
-//   const rows = await connection.query(Query, Params);
-
-//   return rows;
-// };
-
-// //id 유효성 검사
-// exports.isValidMovieId = async function(connection,id){
-//   const Query = `SELECT * FROM movieinfo where id = ?;`; //실제 조회하고자하는 db쿼리문
-//   const Params = [id];
-  
-//   const rows = await connection.query(Query, Params);
-//   if(rows<1){
-//     return false; //유효하지 않은 id가 들어옴
-//   }
-
-//   return true;
-// }
-
-// //업데이트 API
-// exports.updateMovies = async function (connection,id,title,genr,director,price) {
-//   const Query = `UPDATE movieinfo set
-//   title = ifnull(?,title),
-//   genr = ifnull(?,genr),
-//   director = ifnull(?,director),
-//   price = ifnull(?,price) WHERE id = ?;`; //실제 조회하고자하는 db쿼리문
-//   const Params = [title,genr,director,price,id]; //Parmas가 title인자(?로이동)로 전달됌
-
-//   const rows = await connection.query(Query, Params);
-
-//   return rows;
-// };
-
-// //삭제 API
-// exports.deleteMovie = async function(connection,id){
-//   const Query = `delete from movieinfo where id = ?;`
-//   const Params = [id];
-
-//   const rows = await connection.query(Query, Params);
-
-//   return rows;
-// }
-
-
 //조회 API 
-exports.selectRestaurants = async function (connection,category) {
+exports.selectRestaurants = async function (connection, category) {
   const selectAllRestaurantsQuery = `SELECT title,address,URL,category FROM FoodMap WHERE status='A';`; //실제 조회하고자하는 db쿼리문
   const selectRestaurantsByCategoryQuery = `SELECT title,address,URL,category FROM FoodMap where status='A' and category = ?;`; //실제 조회하고자하는 db쿼리문
   const Params = [category]; //Parmas가 title인자(?로이동)로 전달됌
 
   let Query;
-  if(!category){ //만약 title이 null값이라면 모든 영화정보를 조회
+  if (!category) { //만약 title이 null값이라면 모든 영화정보를 조회
     Query = selectAllRestaurantsQuery;
   }
-  else{ 
+  else {
     Query = selectRestaurantsByCategoryQuery;
   }
 
@@ -88,21 +20,21 @@ exports.selectRestaurants = async function (connection,category) {
 };
 
 //회원가입
-exports.insertUsers = async function(connection,userID,password,nickname){
+exports.insertUsers = async function (connection, userID, password, nickname) {
   const Query = `insert into Users(userID,password,nickname) values (?,?,?);`
-  const Params = [userID,password,nickname];
+  const Params = [userID, password, nickname];
 
-  const rows = await connection.query(Query,Params);
+  const rows = await connection.query(Query, Params);
 
   return rows;
 }
 
 //로그인 
-exports.isValidUsers = async function(connection,userID,password){
+exports.isValidUsers = async function (connection, userID, password) {
   const Query = `select userIdx,nickname from Users where userID = ? and password = ? and status = 'A';`
-  const Params = [userID,password];
+  const Params = [userID, password];
 
-  const rows = await connection.query(Query,Params);
+  const rows = await connection.query(Query, Params);
 
   return rows;
 }
--- Back/src/routes/indexRoute.js
+++ Back/src/routes/indexRoute.js
@@ -2,32 +2,15 @@ module.exports = function (app) {
   const index = require("../controllers/indexController"); //app.get callback function
   const jwtMiddleware = require("../../config/jwtMiddleware");
 
-  // 라우터 정의
-  // app.HTTP메서드(uri, 컨트롤러 콜백함수)
-  //app.get("/dummy",index.example);
-
-  // //movieinfo table 조회(Read API-CRUD 중 하나)
-  // //id로 접근시, uri는 '/:id' 형식으로 작성
-  // app.get("/movies",index.readMovies);
-
-  // //영화 생성(Create) API
-  // app.post("/movies",index.createMovie);
-
-  // //영화 정보 업데이트(Update) API
-  // app.patch("/movies/:id",index.updateMovie);
-
-  // //영화 삭제 API
-  // app.delete("/movies/:id",index.deleteMovie);
-
   //레스토랑 전체 조회
-  app.get("/restaurants",index.readRestaurants);
+  app.get("/restaurants", index.readRestaurants);
 
   //회원가입
-  app.post("/sign-up",index.createUsers);
+  app.post("/sign-up", index.createUsers);
 
   //로그인
-  app.post("/sign-in",index.createJwt);
+  app.post("/sign-in", index.createJwt);
 
   //로그인 유지(토큰 검증)
-  app.get("/jwt",jwtMiddleware,index.readJwt);
+  app.get("/jwt", jwtMiddleware, index.readJwt);
 };
